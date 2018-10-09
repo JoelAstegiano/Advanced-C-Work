39c313d289491aefbfecbd08ea4bcc3dc14453a6
SirJoelingtonIII <32949172+SirJoelingtonIII@users.noreply.github.com>
2018-10-08 19:13:38 -0500

    Mod 6 / 7

---
+++ Joel_Astegiano_C#_Module06/Joel_Astegiano_Module06/Joel_Astegiano_Module06/Square.cs
@@ -0,0 +1,8 @@
+﻿namespace Joel_Astegiano_Module06
+{
+    class Square : Rectangle
+    {
+
+    }
+  
+}
---
+++ Joel_Astegiano_C#_Module07/Joel_Astegiano_CS_Mod07/Joel_Astegiano_WinForm_Mod07/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Joel_Astegiano_WinForm_Mod07
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += Button1_ClickOther;
+        }
+
+        private void Button1_ClickOther(object sender, EventArgs e)
+        {
+            label2.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+        }
+    }
+}
